ec7906e8b439ce5a3d6595107d359d9dd5fea8e0
Tim Hemel <tim@securesoftware.nl>
2016-09-04 20:07:32 +0200

    Scripts to test Gremlin queries against an octopus server.

---
+++ joshi.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import sys,re
+from octopus.server.DBInterface import DBInterface
+
+class JoshiApp:
+    def __init__(self):
+        parser = ArgumentParser()
+        parser.add_argument('-p','--project',help='project to execute')
+        parser.add_argument('-r','--raw',action='store_true',default=False,help='print raw response')
+        parser.add_argument('file',help='execute script from file instead of stdin')
+        self.args = parser.parse_args()
+    @staticmethod
+    def _matchShebang(l):
+        m = re.match(r'#!',l)
+        return m
+    @staticmethod
+    def _matchInclude(l):
+            m = re.match(r'^#include (.*)$',l)
+            if m: return m.group(1)
+            return None
+    @staticmethod
+    def _parseScript(f):
+        lines = []
+        for l in f.readlines():
+            l = l.rstrip('\r\n')
+            m = __class__._matchShebang(l)
+            if m:
+                continue
+            m = __class__._matchInclude(l)
+            if m:
+                incf = open(m,"r")
+                inclines = __class__._parseScript(incf)
+                lines.extend(inclines)
+                continue
+            lines.append(l)
+        return lines
+    def run(self):
+        if self.args.file != None:
+            f = open(self.args.file,"r")
+        else:
+            f = sys.stdin
+        lines = __class__._parseScript(f)
+        query = "\n".join(lines)
+        db = DBInterface()
+        if self.args.raw:
+            db.disable_json()
+        db.connectToDatabase(self.args.project)
+
+        result = db.runGremlinQuery(query)
+        for x in result:
+            print(x)
+        db.runGremlinQuery("quit")
+
+if __name__=="__main__":
+    JoshiApp().run()
+    sys.exit()
+
