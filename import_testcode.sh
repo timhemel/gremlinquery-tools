22bc532b9dac04d168e25f115ac4af3f80dfa575
Tim Hemel <tim@securesoftware.nl>
2016-09-04 21:13:23 +0200

    Script to import a directory of source file into Octopus.

---
+++ import_testcode.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Usage:
+#
+#    import_testcode.sh /path/to/testcodedir
+#
+
+codedir_parent=`dirname "$1"`
+project_name=`basename "$1"`
+
+
+tar -zcvf "$project_name".tar.gz -C "$codedir_parent" "$project_name" && \
+joern-delete-project "$project_name".tar.gz && \
+joern-import "$project_name".tar.gz && \
+rm "$project_name".tar.gz
+
