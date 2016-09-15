71351d538233cf5615f9d56cc82bfea5d037c831
Tim Hemel <tim@securesoftware.nl>
2016-09-15 17:35:06 +0200

    testcode for callToAssigns.

--- testdata/test_ast.c
+++ testdata/test_ast.c
@@ -8,3 +8,12 @@ void test_funccall (char *name)
 	fprintf(STDERR, "Hello %s!\n", name);
 }
 
+void test_funccall_from_assignment ()
+{
+	x = 45 + 2*sqrt(9);
+}
+
+void test_funccall_from_nested_assignment ()
+{
+	x = 45 + (a = 5 + sqrt(9));
+}
